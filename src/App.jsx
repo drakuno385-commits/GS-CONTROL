fb7e2e34b93213631712cf5a0baf456c03d92b05
Gabriel <Drakuno385@gmail.com>
2026-08-10 10:59:50 -0300

    Implement Code Splitting with React.lazy and Suspense for extreme performance boost

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, Suspense, lazy } from 'react';
 import Papa from 'papaparse';
 import { supabase } from './supabaseClient';
 import { 
